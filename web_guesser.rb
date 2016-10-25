773217f094b6944378f0b369fd07e39e86090453
Westw00d <westerhout_tom@hotmail.com>
2016-10-25 22:37:13 +0200

    Added stylesheet.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,60 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-x = rand(0..100)
-
 get '/' do
-	"The secret number is #{x}"
+	guess = params['guess'].to_i
+	@cheat = cheat_mode(params['cheat'])
+	@message = check_guess(guess)
+	$remaining_guesses -= 1
+	proces_tracker(guess)
+	erb :index, :locals => {:random_number => $random_number,
+							:remaining_guesses => $remaining_guesses,
+							:guess => params['guess'],
+							:announcement => @announcement,
+							:message => @message,
+							:cheat => @cheat}
+end
+
+$random_number = rand(0..100)
+$remaining_guesses = 5
+
+def proces_tracker(guess)
+	if guess == $random_number
+		reset
+		@announcement = "You got it right! A new game has been started."
+	elsif $remaining_guesses == 0
+		reset
+		@announcement = "No more guesses left. A new game has been started."
+	end
+end
+
+def reset
+	$remaining_guesses = 5
+	$random_number = rand(0..100)
+end
+
+def cheat_mode(input)
+	if input
+		"Cheat: #{$random_number}"
+	end
+end
+
+def check_guess(guess)
+	if guess == "".to_i
+		""
+	elsif guess > $random_number
+		if guess > $random_number + 5
+			"Way too high!"
+		else
+			"Too high!"
+		end
+	elsif guess < $random_number
+		if guess < $random_number - 5
+			"Way too low!"
+		else
+			"Too low!"
+		end
+	elsif guess == $random_number
+		"The secret number is #{$random_number}"
+	end
 end
